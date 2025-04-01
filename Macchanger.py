d1cd6ad009728077b7fc329e028a6a1bf39c7507
Vasu <64682548+Officerwasu@users.noreply.github.com>
2025-04-02 00:04:46 +0530

    Add files via upload

--- Macchanger.py
+++ Macchanger.py
@@ -1,58 +1,71 @@
 #!/usr/bin/env python3
+
 import subprocess
-import optparse
-import re
 import random
-
-def Macchanger(interface, mac_addr):
-    print("[!]You selected " + interface)
-    print("[+]Changing MAC address for " + interface + " to " + mac_addr)
-    subprocess.run(["ifconfig", interface, "down"])
-    subprocess.run(["ifconfig", interface, "hw", "ether", mac_addr])
-    subprocess.run(["ifconfig", interface, "up"])
-
-def theparser():
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address for")
-    parser.add_option("-m", "--mac", dest="mac_addr", help="New MAC address")
-    parser.add_option("-r", "--random", action="store_true", dest="random_mac", help="Generate a random MAC address")
-    (parsoptions, parsarguments) = parser.parse_args()
-    if not parsoptions.interface:
-        parser.error("[X]You did not specify an interface, use --help for more info")
-    elif not parsoptions.mac_addr and not parsoptions.random_mac:
-        parser.error("[X]Please enter a MAC address or use --random, use --help for more info")
-    return parsoptions
-
-def readmac(interface):
-    check = subprocess.check_output(["ifconfig", interface])
-    check = check.decode('utf-8')
-    check_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", check)
-    if check_result:
-        return check_result.group(0)
-    else:
-        print("[x]MAC address not found.")
+import re
+import optparse
 
 def generate_random_mac():
+    """Generates a random MAC address."""
     mac = [0x00, 0x16, 0x3e,
            random.randint(0x00, 0x7f),
            random.randint(0x00, 0xff),
            random.randint(0x00, 0xff)]
     return ':'.join(map(lambda x: "%02x" % x, mac))
 
-parsoptions = theparser()
+def is_valid_mac(mac_address):
+    """Checks if a MAC address is valid."""
+    return bool(re.match(r'^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$', mac_address))
 
-macnow = readmac(parsoptions.interface)
-print("Mac Before = ", macnow)
+def change_mac(interface, mac_address):
+    """Changes the MAC address of a network interface."""
+    try:
+        subprocess.run(["ifconfig", interface, "down"], check=True)
+        subprocess.run(["ifconfig", interface, "hw", "ether", mac_address], check=True)
+        subprocess.run(["ifconfig", interface, "up"], check=True)
+        print(f"[+] MAC address of {interface} changed to {mac_address} successfully.")
 
-if parsoptions.random_mac:
-    random_mac = generate_random_mac()
-    Macchanger(parsoptions.interface, random_mac)
-    parsoptions.mac_addr = random_mac #update parsoptions.mac_addr to the random mac.
-else:
-    Macchanger(parsoptions.interface, parsoptions.mac_addr)
+    except subprocess.CalledProcessError as e:
+        print(f"[-] Command execution failed: {e}")
+        if "Operation not permitted" in str(e):
+            print("[-] Permission denied. Try running as root (sudo).")
+        elif "No such device" in str(e):
+            print(f"[-] Interface '{interface}' not found.")
+        else:
+            print("[-] Check interface name, MAC address, and permissions.")
 
-macnow = readmac(parsoptions.interface)
-if macnow == parsoptions.mac_addr:
-    print("[✔️] MAC address was sucessfully changed to ", macnow)
-else:
-    print("[x] MAC address did not get changed.")
+    except FileNotFoundError:
+        print("[-] ifconfig command not found. Ensure net-tools is installed.")
+
+    except Exception as e:
+        print(f"[-] An unexpected error occurred: {e}")
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    parser.add_option("-r", "--random", action="store_true", dest="random_mac", help="Generate a random MAC address")
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        if options.random_mac:
+            print(generate_random_mac()) # only print the mac address if -r is given.
+        else:
+            parser.error("[-] Please specify an interface, use --help for more info.")
+
+    interface = options.interface
+
+    if options.random_mac:
+        mac_address = generate_random_mac()
+        print(f"[+] Generating random MAC: {mac_address}")
+    elif options.new_mac:
+        mac_address = options.new_mac
+        if not is_valid_mac(mac_address):
+            print("[-] Invalid MAC address format.")
+            exit(1)
+    else:
+        parser.error("[-] Please specify a new MAC address or use --random, use --help for more info.")
+
+    print(f"[!] You selected {interface}")
+    print(f"[+] Changing MAC address for {interface} to {mac_address}")
+    change_mac(interface, mac_address)
